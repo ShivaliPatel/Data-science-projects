384b6312bef6387ba66ebb87df3d67e3df4b63a0
Shivali Patel <shivuptl@gmail.com>
2020-03-03 10:19:56 -0500

    Generated functions and constants file in TCM

---
+++ Telco_customer_churn/Modules/Constants.py
@@ -0,0 +1,5 @@
+#constant file
+
+random_state = 42
+plotColor = ['b','g','r','m','c', 'y']
+markers = ['+','o','*','^','v','>','<']
---
+++ Telco_customer_churn/Modules/Helper_function.py
@@ -0,0 +1,465 @@
+import pandas as pd
+import numpy as np
+import math as math
+import random
+from sklearn.utils import shuffle
+import sklearn.preprocessing  as prepro
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.utils import resample
+from sklearn.cluster import KMeans
+from imblearn.over_sampling import SMOTE
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.lines as mlines
+import Constants as const
+
+
+#default configuration
+
+
+sns.set(style='whitegrid')
+
+
+
+# Create Data class with method _load_data 
+class Data:
+    def __init__(self):
+        print("Data object initiated")
+    
+    def load_data(self,filepath,format='csv'):
+        if format == 'csv':
+            return pd.read_csv(filepath)
+        elif format == 'xlsx':
+            return pd.read_excel(filepath)
+
+def data_transformation_renaming(df_raw):
+    df_cal = df_raw.copy()
+    
+    df_cal.rename(columns={'gender':'Gender'
+                       ,'customerID':'CustomerID'
+                       ,'Contract':'ContractType'
+                       ,'InternetService':'InternetServiceType'
+                       ,'tenure':'Tenure'
+                      }
+              ,inplace=True)
+
+
+    df_cal['Partner'] = df_cal.Partner.map({'Yes':1,'No':0})
+    df_cal['Dependents'] = df_cal.Dependents.map({'Yes':1,'No':0})
+
+    df_cal['PhoneService'] = df_cal.PhoneService.map({'Yes':1,'No':0})
+    df_cal['MultipleLines'] = df_cal.MultipleLines.map({'Yes':1,'No':0,'No phone service':0})
+
+    df_cal['InternetService'] = df_cal.InternetServiceType.map({'DSL':1,'Fiber optic':1,'No':0})
+    df_cal['OnlineSecurity'] = df_cal.OnlineSecurity.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['OnlineBackup'] = df_cal.OnlineBackup.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['DeviceProtection'] = df_cal.DeviceProtection.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['TechSupport'] = df_cal.TechSupport.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['StreamingTV'] = df_cal.StreamingTV.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['StreamingMovies'] = df_cal.StreamingMovies.map({'Yes':1,'No':0,'No internet service':0})
+    df_cal['PaperlessBilling'] = df_cal.PaperlessBilling.map({'Yes':1,'No':0})
+    df_cal['Churn'] = df_cal.Churn.map({'Yes':1,'No':0})
+    
+    # Data mining
+    df_cal['IsContracted'] = df_cal.ContractType.map({'One year':1,'Two year':1,'Month-to-month':0})
+    
+    # Data transformation
+    
+    # Converting TotalCharges into Numeric, but some of the records are empty, so first we need to deal with them.
+    df_cal.loc[df_cal['TotalCharges']==' ','TotalCharges'] = np.nan
+    
+    # First we convert TotalCharges to float and then replace with tenure * monthly charges
+    df_cal['TotalCharges'] = df_cal['TotalCharges'].astype('float64')
+    df_cal.loc[df_cal['TotalCharges'].isnull()==True,'TotalCharges'] = df_cal['MonthlyCharges'] * df_cal['Tenure']
+    
+
+    return df_cal
+
+
+def generate_bar_graph(x, y, x_title, y_title, chart_title,color=const.plotColor):
+    fig, ax = plt.subplots()
+    ax.bar(range(len(x))
+       , y
+       , width = 0.75
+       , color=color
+        , alpha = 0.6) 
+
+    # stopping alphabetical sorting of graph
+    plt.xticks(range(len(x)),x)
+    plt.title(chart_title, fontsize=14)
+    plt.xlabel(x_title,fontsize=13)
+    plt.ylabel(y_title,fontsize=13)
+    plt.grid(b=False)
+    plt.yticks(fontsize=0)
+    plt.ylim(top=1)
+
+    
+    # Visible x - axis line
+    for spine in plt.gca().spines.values():
+        spine.set_visible(False) if spine.spine_type != 'bottom' else spine.set_visible(True)
+    
+    # Display label for each plot
+    for i,v in (enumerate(y)):
+        ax.text(i,v+0.05,str(round((v*100),2))+'%',fontsize=13,ha='center')
+    
+    plt.show()
+
+    
+def generate_heatmap_graph(corr, chart_title, mask_uppertri=False ):
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = mask_uppertri
+
+    fig,ax = plt.subplots(figsize=(12,12))
+    sns.heatmap(corr
+                , mask = mask
+                , square = True
+                , annot = True
+                , annot_kws={'size': 10.5, 'weight' : 'bold'}
+                , cmap=plt.get_cmap("YlOrBr")
+                , linewidths=.1)
+    plt.title(chart_title, fontsize=14)
+    plt.show()
+
+    
+def create_data_label(ax):
+    for bar in ax.patches:
+            ax.text(bar.get_x() + bar.get_width()/ 2
+                    , bar.get_height() + 0.01
+                    , str(round(100 * bar.get_height(),2)) + '%'
+                    , ha = 'center'
+                    , fontsize = 13)
+            
+def categorical_var_churn_dist(data, cols, distribution_col):
+    
+    for i,feature in enumerate(cols):
+        
+        feature_summary = data[feature].value_counts(normalize=True).reset_index(name='Percentage')
+        
+        plt_cat = sns.catplot(x=feature
+                , y='Percentage'
+                , data = feature_summary
+                , col=distribution_col
+                , kind='bar'
+                , aspect = 0.8
+                , palette = const.plotColor
+                , alpha = 0.6)
+        
+        if feature == 'PaymentMethod':
+            plt_cat.set_xticklabels(rotation= 65, horizontalalignment = 'right')
+        
+        
+        for ax1, ax2 in plt_cat.axes:
+            create_data_label(ax1)
+            create_data_label(ax2)
+        
+        
+        plt.ylim(top=1)
+        plt.subplots_adjust(top = 0.9)
+        plt.gcf().suptitle(feature+" distribution",fontsize=14)
+    plt.show()
+
+
+def numerical_distribution(df_cal,feature):
+    fig = plt.figure(figsize=(15,10))
+    
+    plt.subplot(2,1,1)
+    ax = sns.kdeplot(df_cal[feature]
+                     , color = 'g'
+                     , shade = True)
+    
+    title_str = "Original " +feature + " Distribution"
+    plt.title(title_str)
+    
+    plt.subplot(2,1,2)
+    ax = sns.kdeplot(df_cal.loc[(df_cal['Churn']==1),feature]
+                     , color = 'g'
+                     , shade = True
+                     , label='Chrun')
+
+    ax = sns.kdeplot(df_cal.loc[(df_cal['Churn']==0) ,feature]
+                     , color = 'b'
+                     , shade = True
+                     , label='No chrun')
+    
+    title_str = feature + " Distribution: Churn vs No churn"
+    plt.title(title_str)
+    plt.show()
+
+    
+def create_elbow_curve(data):
+    df_kmeans_data = data
+
+    k = range(1,10)
+    kmeans = [KMeans(n_clusters=i) for i in k]
+
+    score = [kmeans[i].fit(df_kmeans_data).score(df_kmeans_data)  for i in range(len(kmeans))]
+
+    plt.figure(figsize=(10,6))
+    plt.plot(k,score)
+    plt.xlabel("Clusters")
+    plt.ylabel("Score")
+    plt.title("Elbow curve",fontsize=15)
+
+    
+def create_kmeans_cluster_graph(df_cal, data, n_clusters, x_title, y_title, chart_title):
+    
+    kmeans = KMeans(n_clusters=n_clusters # No of cluster in data
+                    , random_state = const.random_state # Selecting same training data
+                   ) 
+
+    kmeans.fit(data)
+    kmean_colors = [const.plotColor[c] for c in kmeans.labels_]
+
+
+    fig = plt.figure(figsize=(12,8))
+    plt.scatter(x= x_title + '_norm'
+                , y= y_title + '_norm'
+                , data=data 
+                , color=kmean_colors # color of data points
+                , alpha=0.25 # transparancy of data points
+               )
+
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+
+    plt.scatter(x=kmeans.cluster_centers_[:,0]
+                , y=kmeans.cluster_centers_[:,1]
+                , color='black'
+                , marker='X' # Marker sign for data points
+                , s=100 # marker size
+               )
+    
+    plt.title(chart_title,fontsize=15)
+    plt.show()
+    
+    return kmeans.fit_predict(df_cal[df_cal.Churn==1][[x_title+'_norm', y_title +'_norm']])
+
+
+def extract_highest_in_cluster(df_cal, df_cluster, feature, tenure_charges_cluster_df ):
+    
+    df = df_cal.copy()
+    feature_churn_dist = df[(df['Churn']==1)][feature].value_counts(normalize=True).reset_index()
+    feature_churn_dist.columns = [feature,'Percentage']
+    feature_cluster_dist = df_cluster[feature].value_counts(normalize=True).to_frame()
+    feature_cluster_dist.columns = ['Percentage']
+    feature_cluster_dist = feature_cluster_dist.reset_index()
+    feature_cluster_dist_new = feature_cluster_dist.copy()
+    
+    tenure_MonthlyCharges_df = df_cal[df_cal['Churn']==1].groupby(['Cluster',feature],as_index=False)['Tenure','MonthlyCharges'].mean()
+    for i,cluster in enumerate(feature_cluster_dist_new['Cluster'].unique()):
+        for i, label in enumerate(feature_churn_dist[feature].unique()):
+            cluster_val = feature_cluster_dist_new[(feature_cluster_dist_new['Cluster']==cluster) & (feature_cluster_dist_new[feature]==label)]['Percentage']
+            feature_val = feature_churn_dist[feature_churn_dist[feature] == label]['Percentage']
+            
+            if((len(feature_val.values) > 0) & (len(cluster_val.values) > 0)) :
+                if((feature_val.values[0] < cluster_val.values[0])):
+                    
+                    tenure_charges_cluster_df = tenure_charges_cluster_df.append(pd.DataFrame({'Category':feature
+                            , 'Label': ("Not have a "+ feature) if (df_cal[feature].dtypes == 'int64') & (label == 0) else (("Have a "+feature) if (df_cal[feature].dtypes == 'int64') & (label == 1) else label)
+                            , 'Percentage': cluster_val.values[0]
+                            , 'Cluster' : cluster
+                            , 'Avg_Tenure': round(tenure_MonthlyCharges_df[(tenure_MonthlyCharges_df['Cluster']==cluster) & (tenure_MonthlyCharges_df[feature]==label) ]['Tenure'].values[0],2)
+                            , 'Avg_MonthlyCharges': round(tenure_MonthlyCharges_df[(tenure_MonthlyCharges_df['Cluster']==cluster) & (tenure_MonthlyCharges_df[feature]==label) ]['MonthlyCharges'].values[0],2)
+                            , 'Represent_in_graph': 0 if (label == 0) | (label == 'No') else 1
+                            , 'Label_in_graph' :  feature if (df_cal[feature].dtypes == 'int64') else label
+                           }
+                        , index = [len(tenure_charges_cluster_df)])
+                        )
+                    
+                    
+                    
+    return tenure_charges_cluster_df
+
+
+from adjustText import adjust_text
+
+def create_kmeans_cluster_with_label(data, cluster_visualize_gp, x_title, y_title, chart_title):
+    
+    legend_list = []
+    category_color = cluster_visualize_gp[['Category']].drop_duplicates().reset_index()
+    annotations = []
+    
+    fig, ax = plt.subplots(figsize=(12,8))
+    plt.scatter(x= x_title 
+                , y= y_title
+                , data=data 
+                , color=[const.plotColor[c] for c in data.Cluster] # color of data points
+                , alpha=0.25 # transparancy of data points
+                , s = 15
+               )
+
+    for i,txt in enumerate(cluster_visualize_gp['Label_in_graph']):
+        annotations.append(ax.text(cluster_visualize_gp['Avg_Tenure'][i]
+                                , cluster_visualize_gp['Avg_MonthlyCharges'][i]
+                                , txt
+                                , fontsize = 13
+                                , weight="bold"))
+        ax.scatter(x=cluster_visualize_gp['Avg_Tenure'][i]
+            , y=cluster_visualize_gp['Avg_MonthlyCharges'][i]
+            , color = const.plotColor[category_color[category_color['Category'] == cluster_visualize_gp['Category'][i]].index[0]]
+            , label = cluster_visualize_gp['Label_in_graph'][i]
+            , marker = const.markers[category_color[category_color['Category'] == cluster_visualize_gp['Category'][i]].index[0]]
+            , s=120 # marker size
+           )
+    
+    for key,i in enumerate(category_color.Category.values):
+        legend_list.append(mlines.Line2D([]
+                            , []
+                            , linestyle= 'None'
+                            , color = const.plotColor[key]      
+                            , markersize = 10
+                            , marker = const.markers[key]
+                            , label= i))
+    
+    adjust_text(annotations
+                ,x=cluster_visualize_gp['Avg_Tenure']
+                ,y=cluster_visualize_gp['Avg_MonthlyCharges'])
+    
+    plt.legend(handles=legend_list
+              , loc = 'lower right')
+    plt.xlabel(x_title)
+    plt.ylabel(y_title)
+    plt.title(chart_title,fontsize=15)
+    plt.show()
+
+
+def shuffle_data(df):
+    return shuffle(df).reset_index().drop('index'
+                                          , axis = 1)
+
+def one_hot_encoding(df,feature):
+    return pd.concat([df
+                      , pd.get_dummies(df[feature])]
+                     , axis = 1)
+
+def merge_df(data, features):
+    merged_df = pd.DataFrame()
+    for feature in features:
+        merged_df = pd.concat([merged_df, data[feature]]
+                              , axis = 1)
+    
+    return merged_df
+
+def normalize_feature(feature):
+    return prepro.StandardScaler().fit_transform(feature)
+
+	
+def normalize_df(data, cols):
+    for col in cols:
+        data[col] = normalize_feature(data[[col]])
+        
+    return data    
+
+def create_model_df(df, col_cols, nom_cols, binary_cols, num_cols, target_col):
+    # Preparing data for modelling
+    
+    df_model = merge_df(df , [col_cols, num_cols, target_col])
+    df_model = shuffle_data(df_model)
+    df_model = one_hot_encoding(df_model, nom_cols)
+    
+    df_model.drop(nom_cols, axis = 1, inplace = True)
+    
+    df_model = normalize_df(df_model, num_cols)
+    
+    return (df_model.loc[:, ~df_model.columns.isin(target_col)]
+            , df_model[target_col].values.flatten())
+    
+def train_test_df(feature, target, test_size):
+    return train_test_split(feature
+                     ,target
+                     ,test_size= test_size
+                     ,random_state= const.random_state)
+
+
+def cross_validation_score(clsModels, clsModelsNm, clsSample, scoreMatrix):
+    clsScore= {}
+    for i,model in enumerate(clsModels):
+        print("===============================================")
+        print(clsModelsNm[i])
+
+        for j, sample in enumerate(clsSample):
+            print("************************************************")
+            print(sample[2])
+
+            if not clsScore.get(sample[2]):
+                clsScore[sample[2]] = {}
+
+            if not clsScore[sample[2]].get(clsModelsNm[i]):
+                clsScore[sample[2]][clsModelsNm[i]] = []
+
+            for score in scoreMatrix:
+                scores = cross_val_score(model, sample[0], sample[1], cv=5 , scoring = score)
+                print(score, " score:", scores.mean())
+
+                clsScore[sample[2]][clsModelsNm[i]].append(scores.mean())
+    
+    return clsScore
+
+def generate_cross_valid_graph(clsScore,scoreMatrix):
+    for i, sample in enumerate(clsScore.keys()):
+        
+        for j, model in enumerate(clsScore[sample].keys()):
+            plt.plot(clsScore[sample][model]
+                    ,color = const.plotColor[j])
+            
+        plt.xticks(range(len(scoreMatrix)),scoreMatrix)
+        plt.title(sample, fontsize = 14)
+        plt.xlabel("Matrix", fontsize = 13)
+        plt.ylabel("Score", fontsize = 13)
+        plt.legend(clsScore[sample].keys())
+        plt.show()
+
+        
+def Parameter_tunning(x, y, models, clsModelsNm, parameters, score):
+    
+    tuned_params = {}
+    for i,model in enumerate(models):
+        print(clsModelsNm[i])
+        grid = GridSearchCV(estimator = model 
+                            , cv = 5
+                            , param_grid = parameters[clsModelsNm[i]]
+                            , scoring = score)
+        grid.fit(x,y)
+        print(grid.best_score_)
+        print(grid.best_params_)
+        tuned_params[clsModelsNm[i]] = {'params':grid.best_params_}
+    
+    return tuned_params
+
+
+def model_evaluation(x_train, y_train, x_test, y_test, models, clsModelsNm ):
+    
+    predicted_val = {}
+    for i, model in enumerate(clsModelsNm):
+        models[i].fit(x_train,y_train)
+        predicted = models[i].predict(x_test)
+        confusion_matrix_matrix = metrics.confusion_matrix(y_true = y_test
+                                                   ,y_pred = predicted
+                                                   #,normalize = 'true' 
+                                                   )
+        #confusion_matrix_matrix = confusion_matrix_matrix / confusion_matrix_matrix.astype(np.float).sum(axis=1)
+        
+        print("***********",clsModelsNm[i], "*************")
+        print(metrics.classification_report(y_test, predicted))
+        print("*******************************************")
+        print("Log loss score", round(metrics.log_loss(y_test,models[i].predict_proba(x_test)[:,1]),2))
+        print("*******************************************")
+        print("Confusion matrix")
+        sns.heatmap(confusion_matrix_matrix
+                    , annot=True
+                    , fmt="d"
+                   )
+        plt.xlabel("Predicted label")
+        plt.ylabel("Actual label")
+        plt.show()
+        print("*******************************************")
+        
+        predicted_val[model] = predicted
+
+def Outlier_boxplot(df, col):
+        sns.boxplot(x=df[col])
+        plt.show()
+    
